1fe953ad3a2f104d3a1c2bfa92508e4939d51a61
svv0003 <ssetoo4689@naver.com>
2025-07-30 16:07:51 +0900

    Ajax를 활용한 로그인 로그아웃 실습

--- js/12_Ajax_로그인.js
+++ js/12_Ajax_로그인.js
@@ -7,7 +7,7 @@ $(function () {
   // 로그인 버튼을 클릭했을 때 ajax 작동
   $("#로그인기능").click(function (e) {
     // button 제출 방지
-    e.preventDefault(); // submit 잠시 멈춤 -> 정규식이나 비밀번호 아이디 일치하는지 확인하고 제출
+    e.preventDefault();     // submit 잠시 멈춤 -> 정규식이나 비밀번호 아이디 일치하는지 확인하고 제출
     // const username = document.getElementById("username").value;
     const userID = $("#username").val();
     const password = $("#password").val();
@@ -37,25 +37,32 @@ $(function () {
 
         // users는 배열 형태
         // users.username (X) -> users[username] (O)
-        if (data.users[userID].password === password) {
-          $("#result").html(
-            `로그인 성공! 환영합니다. ${data.users[userID].name}님`
-          );
-          $("#로그인기능").hide();
-          $("#로그아웃기능").show();
+        if (data.users[userID]{                                   // 사용자가 존재할 때
+          if (data.users[userID].password === password) {         // 사용자도 존재하고, 비밀번호도 일치할 때
+            $("#result").html(
+              `로그인 성공! 환영합니다. ${data.users[userID].name}님`
+            );
+            $("#로그인기능").hide();
+            $("#로그아웃기능").show();
+          } else {                                                 // 사용자가 존재하지만 비밀번호 일치하지 않을 때
+            $("#result").html("일치하는 비밀번호가 없습니다.");
+            
+          }
         } else {
-          $("#result").html("일치하는 아이디나 비밀번호가 없습니다.");
-        }
-      },
-      error: function () {
+          $("#result").html("존재하는 아이디가 없습니다.")
+      }, error: function () {
         alert("데이터를 가져오는데 실패했습니다.");
       },
     });
   });
-
+  
   $("#로그아웃기능").click(function (e) {
     // 로그아웃을 진행할 경우 로그인기능 show, 로그아웃기능 hide 설정
+    e.preventDefault();           // 로그아웃 button 태그 내부에 type="submit" 설정되어 있어, 제출 방지용
     $("#로그인기능").hide();
     $("#로그아웃기능").show();
+    $("#userID").val();
+    $("#password").val();
+    $("#result").html("로그아웃이 완료되었습니다.");
   });
 });
