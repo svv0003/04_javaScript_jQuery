3c0471cd27fa72d5971711d74f5a694af7d0e361
svv0003 <ssetoo4689@naver.com>
2025-07-30 15:45:57 +0900

    Ajax를 활용한 로그인 실습

---
+++ js/12_Ajax_로그인.js
@@ -0,0 +1,61 @@
+// 사용자 샘플 데이터
+// DB -> Java -> 프론트엔드로 데이터를 가져오거나
+// 프론트엔드 -> Java -> DB에서 소비자가 요청한 데이터가 존재하는지 확인
+// 존재한다면 DB -> Java -> 프론트엔드로 데이터를 전달 / 없으면 전달할 것이 없음
+
+$(function () {
+  // 로그인 버튼을 클릭했을 때 ajax 작동
+  $("#로그인기능").click(function (e) {
+    // button 제출 방지
+    e.preventDefault(); // submit 잠시 멈춤 -> 정규식이나 비밀번호 아이디 일치하는지 확인하고 제출
+    // const username = document.getElementById("username").value;
+    const userID = $("#username").val();
+    const password = $("#password").val();
+
+    $.ajax({
+      url: "../json/data.json", // json파일이 위치한 곳으로 url 주소 설정
+      method: "GET",
+      dataType: "json",
+      success: function (data) {
+        // case1. 무사히 url에 접속해서 데이터를 가져왔는데 데이터가 존재하지 않는 경우
+        // case2. 데이터가 사용자가 작성한 정보와 일치하지 않아서 데이터를 가져올 수 없는 경우
+        // case3. 데이터가 존재할 경우
+        console.log("성공적으로 json에서 가져온 데이터 확인하기", data);
+        // 만약 json에서 확인한 데이터와 소비자가 작성한 데이터가 일치한다면
+
+        /*
+        JavaScript에서 [], . 으로 데이터를 주고 받을 때의 차이
+        
+        users[abc]  : abc의 변수의 값이 admin인 속성을 찾을 때 사용
+                      -> users 안에 abc라는 변수 이름으로 가져온 값이 존재하는가??
+        users.abc   : 속성명이 고정된 문자열일 때 사용
+                      -> users 안에 abc라는 id가 존재하는가?????????
+
+        예를 들어 username.value 값으로 admin이 들어왔을 경우
+        users[abc] -> users[admin]으로 변경되어 admin과 일치하는 ID를 검색
+        */
+
+        // users는 배열 형태
+        // users.username (X) -> users[username] (O)
+        if (data.users[userID].password === password) {
+          $("#result").html(
+            `로그인 성공! 환영합니다. ${data.users[userID].name}님`
+          );
+          $("#로그인기능").hide();
+          $("#로그아웃기능").show();
+        } else {
+          $("#result").html("일치하는 아이디나 비밀번호가 없습니다.");
+        }
+      },
+      error: function () {
+        alert("데이터를 가져오는데 실패했습니다.");
+      },
+    });
+  });
+
+  $("#로그아웃기능").click(function (e) {
+    // 로그아웃을 진행할 경우 로그인기능 show, 로그아웃기능 hide 설정
+    $("#로그인기능").hide();
+    $("#로그아웃기능").show();
+  });
+});
