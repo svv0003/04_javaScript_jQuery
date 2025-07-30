80a06fdaf5621d55cb1f7e8e046cf4a1db8c44ac
svv0003 <ssetoo4689@naver.com>
2025-07-30 10:19:41 +0900

    기본 시각 효과 메소드 / 애니메이션

---
+++ js/06_애니메이션.js
@@ -0,0 +1,44 @@
+$(() => {
+  // 문제 1: 모달 팝업
+  $("#openModal").click(function () {
+    $("#modalOverlay").fadeIn(300);
+    // HINT: #modalOverlay를 fadeIn(300)으로 나타내세요
+  });
+
+  $("#closeModal").click(function () {
+    $("#modalOverlay").fadeOut(300);
+    // HINT: #modalOverlay를 fadeOut(300)으로 사라지게 하세요
+  });
+
+  $("#modalOverlay").click(function (e) {
+    // HINT: e.target이 자기 자신일 때만 닫기 (이벤트 버블링 방지)
+    /*
+    modalOverlay      : modal이 존재하는 전체 화면 배경
+    modal-content     : 실제 modal 창
+    e.target === this : modalOverlay의 배경 영역을 직접 클릭했을 때
+                        조건이 false인 경우는 modal-content나 내부 요소를 클릭했을 때
+    이벤트 버블링        : 자식 요소에서 발생한 이벤트가 부모 요소로 차례차례 전달되는 현상
+                        에러코드 등의 내용 복사하는 경우를 위해 modal 창은 클릭 꺼짐 방지!
+                        
+    */
+    if (e.target === this) {
+      $("#modalOverlay").fadeOut();
+    }
+  });
+
+  // 문제 2: 탭 메뉴
+  $(".tab-btn").click(function(){
+    // data-tab 속성값을 가져오기
+    // const targetTab = $(".tab-btn").data("tab");
+    const targetTab = $(this).data("tab");
+    // 모든 탭 버튼에서 active 클래스를 제거한 후 현재 클릭한 버튼에만 active 추가하기
+    $(".tab-btn").removeClass("active");
+    $(this).addClass("active");
+    // 모든 .tab-content를 slideUp 하고, 해당 탭만 slideDown 하기
+    // 메소드 .slideUp() .slideDown() 기본 속도는 400ms
+    $(".tab-content").slideUp();
+    $("#" + targetTab).slideDown();
+  })
+  // .data()
+  // $("#" + targetTab)
+});
