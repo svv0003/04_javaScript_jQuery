aa19733b68fa48ff098db7448d926d77e16262d5
svv0003 <ssetoo4689@naver.com>
2025-08-01 10:47:53 +0900

    로그아웃 기능 실습

--- 홈페이지실습/js/index.js
+++ 홈페이지실습/js/index.js
@@ -3,6 +3,7 @@ $(function () {
   // 특별히 메소드 내부에 함수를 작성하지 않고, 단독으로 함수를 작성할 때는
   //  기능명칭();
   $("#loginBtn").click(loginCheck);
+  $("#logoutBtn").click(logoutCheck);
 });
 
 function loginCheck() {
@@ -27,11 +28,16 @@ function loginCheck() {
     `
   );
 
+  // 로그인 성공 시
+  // form-group 숨김 처리, loginBtn 로그아웃 버튼으로 변경
+  // 로그아웃 버튼 클릭 시 form-group 보이기 처리, 로그인 버튼으로 변경
   if (
-    (username === "admin" && password === "1234")
-    ||
+    (username === "admin" && password === "1234") ||
     (username === "user" && password === "1234")
   ) {
+    $(".form-group").hide();
+    $("#loginBtn").hide();
+    $("#logoutBtn").show();
     $("#loginResult").html(
       `
       <div class="success">
@@ -48,3 +54,29 @@ function loginCheck() {
     );
   }
 }
+
+function logoutCheck() {
+  // 입력 필드 초기화
+  $("#username").val("");
+  $("#password").val("");
+
+  // UI 원래대로 복구
+  $(".form-group").show();
+  $("#loginBtn").show();
+  $("#logoutBtn").hide();
+
+  // 로그아웃 완료 메세지
+  $("#loginResult").html(
+    `
+    <div class="success">로그아웃이 완료되었습니다.</div>
+    `
+  );
+
+  // 메세지 나타내고 1초 뒤에는 사라지도록 설정
+  // 일반적으로 소비자가 느끼기엔 3초가 너무 길어서 0.5초, 1초로 많이 설정한다.
+  setTimeout(function(){
+    $("#loginResult").html("");
+  }, 1000);
+}
+
+logoutCheck();
